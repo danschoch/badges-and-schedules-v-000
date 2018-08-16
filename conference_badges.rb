74a29b4ee990243c96031228a5f9fc3904a9b0bf
Daniel Schoch <d.schoch90@gmail.com>
2018-08-16 22:56:30 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,10 +2,13 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
+def batch_badge_creator(speaker_list)
+  badge_array = [
+  speaker_list.collect {|speaker| badge_maker (speaker)}
+]
 end
 
-def assign_rooms(array)
+def assign_rooms(speaker_list)
 end
 
 def printer
